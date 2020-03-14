d698c775d0c6010d7baafd2ecc205fdbe81eb671
ssingh <sandy.boon@gmail.com>
2020-03-14 19:12:26 -0400

    create timeblocks using jquery and bootstrap

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
-//Global vars
-let currentDay = $('#currentDay');
+('use strict');
 
-/* Add tome block rows programatically on load */
+//Global vars
+const currentDay = $('#currentDay');
+const mainContianer = $('#mainContainer');
 
 $(document).ready(function() {
   //set thhe current day
@@ -10,4 +11,58 @@ $(document).ready(function() {
       .local()
       .format('dddd,MMMM,Do')
   );
+
+  //Make sure its empty first.
+  mainContianer.html('');
+  /* Add tome block rows programatically on load */
+  for (let i = 9; i < 18; i++) {
+    // console.log(moment(i.toString()).format('h'));
+    addTimeBlock(i > 12 ? i - 12 : i);
+  }
 });
+
+function addTimeBlock(time) {
+  let timeBlockRow = bootStrapHelper.getBootStrapGridRow(
+    'timeBlockRow',
+    '<div>',
+    'row future time-block'
+  );
+  let hourColumn = bootStrapHelper.getootStrapGridColumn(
+    'hourDiv',
+    '<div>',
+    'col-1 hour'
+  );
+  let hourSpan = bootStrapHelper.createJqueryDomElement('hourSpan', '<span>');
+  hourSpan.text(time);
+  hourColumn.append(hourSpan);
+  timeBlockRow.append(hourColumn);
+  //   console.log(timeBlockRow);
+  let inputDiv = bootStrapHelper.getootStrapGridColumn(
+    'inputDiv',
+    '<div>',
+    'col-10 textarea text-left p-0'
+  );
+  let inputTask = bootStrapHelper.getFormInput(
+    'task',
+    'text',
+    'form-control-lg description p-2 mt-3'
+  );
+  inputDiv.append(inputTask);
+  timeBlockRow.append(inputDiv);
+  let saveBtnDiv = bootStrapHelper.getootStrapGridColumn(
+    'saveBtnDiv',
+    '<div>',
+    'col-1 saveBtn pt-4'
+  );
+  let saveTaskSpan = bootStrapHelper.createJqueryDomElement(
+    'saveTaskSpan',
+    '<span>'
+  );
+  let saveIcon = bootStrapHelper
+    .createJqueryDomElement('saveIcon', '<i>')
+    .attr('class', 'fas fa-lg fa-save');
+  saveTaskSpan.append(saveIcon);
+  saveBtnDiv.append(saveTaskSpan);
+  timeBlockRow.append(saveBtnDiv);
+  mainContianer.append(timeBlockRow);
+}
