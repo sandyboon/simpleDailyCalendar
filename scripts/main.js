5d785eb1474d6f74d51be35857a4111857fb7805
ssingh <sandy.boon@gmail.com>
2020-03-14 21:16:57 -0400

    generate proper IDs for elements for selection

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,9 @@
 //Global vars
 const currentDay = $('#currentDay');
 const mainContianer = $('#mainContainer');
+//IDs reference
+const saveTaskSpanId = 'saveTaskSpan';
+const saveBtnDivId = 'saveBtnDiv';
 
 $(document).ready(function() {
   //set thhe current day
@@ -22,8 +25,11 @@ $(document).ready(function() {
   /* Add tome block rows programatically on load */
   for (let i = 9; i < 18; i++) {
     // console.log(moment(i.toString()).format('h'));
-    addTimeBlock(timeToDisplay(i), getTimeBlockStyle(i, currentHour));
+    addTimeBlock(i, timeToDisplay(i), getTimeBlockStyle(i, currentHour));
   }
+
+  //define savebtn click listener to store data in storage
+  $('.saveBtn').on('click', saveTaskDetails);
 });
 
 function timeToDisplay(i) {
@@ -47,48 +53,62 @@ function getTimeBlockStyle(time, currentHour) {
   }
 }
 
-function addTimeBlock(time, timeBlocStyle) {
+function addTimeBlock(id, time, timeBlocStyle) {
   let timeBlockRow = bootStrapHelper.getBootStrapGridRow(
-    'timeBlockRow',
+    'timeBlockRow' + id,
     '<div>',
     timeBlocStyle
   );
   let hourColumn = bootStrapHelper.getootStrapGridColumn(
-    'hourDiv',
+    'hourDiv' + id,
     '<div>',
     'col-1 hour'
   );
-  let hourSpan = bootStrapHelper.createJqueryDomElement('hourSpan', '<span>');
+  let hourSpan = bootStrapHelper.createJqueryDomElement(
+    'hourSpan' + id,
+    '<span>'
+  );
   hourSpan.text(time);
   hourColumn.append(hourSpan);
   timeBlockRow.append(hourColumn);
   //   console.log(timeBlockRow);
   let inputDiv = bootStrapHelper.getootStrapGridColumn(
-    'inputDiv',
+    'inputDiv' + id,
     '<div>',
     'col-10 textarea text-left p-0'
   );
   let inputTask = bootStrapHelper.getFormInput(
-    'task',
+    'task' + id,
     'text',
     'form-control-lg description p-2 mt-3'
   );
   inputDiv.append(inputTask);
   timeBlockRow.append(inputDiv);
   let saveBtnDiv = bootStrapHelper.getootStrapGridColumn(
-    'saveBtnDiv',
+    saveBtnDivId + id,
     '<div>',
     'col-1 saveBtn pt-4'
   );
   let saveTaskSpan = bootStrapHelper.createJqueryDomElement(
-    'saveTaskSpan',
+    saveTaskSpanId + id,
     '<span>'
   );
   let saveIcon = bootStrapHelper
-    .createJqueryDomElement('saveIcon', '<i>')
+    .createJqueryDomElement('saveIcon' + id, '<i>')
     .attr('class', 'fas fa-lg fa-save');
   saveTaskSpan.append(saveIcon);
   saveBtnDiv.append(saveTaskSpan);
   timeBlockRow.append(saveBtnDiv);
   mainContianer.append(timeBlockRow);
+}
+
+function saveTaskDetails() {
+  const taskInput = $(this)
+    .siblings()
+    .has('input')
+    .children()
+    .first();
+  const taskId = taskInput.attr('id');
+  let taskDetails = taskInput.val();
+  //   localStorage.localStorage.setItem('scores',
 }
